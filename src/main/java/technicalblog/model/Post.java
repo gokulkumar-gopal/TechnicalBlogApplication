f4a7c8018c67ce93ee6e5a809bebf4e6d2e871cb
Gopal <gokul.gopal@bt.com>
2020-09-14 20:08:49 +0530

    first milestone

---
+++ src/main/java/technicalblog/model/Post.java
@@ -0,0 +1,34 @@
+package technicalblog.model;
+
+import java.util.Date;
+
+public class Post {
+
+    private String title;
+    private String body;
+    private String date;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
